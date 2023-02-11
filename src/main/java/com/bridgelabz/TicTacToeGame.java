32ff53949be77ce859875fef2752f5915743ce4c
pranit67 <pranit67@gmail.com>
2023-02-11 15:13:01 +0530

    Added: code For Display Current Board

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -1,18 +1,18 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToeGame {
-    static Scanner scan = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+    static String[] board = new String[10];
 
     public static void main(String[] args) {
-        TicTacToeGame obj = new TicTacToeGame();
         createBoard();
         playerInput();
+        showBoard();
     }
 
     public static void createBoard() {
-        String[] board = new String[10];
         for (int i = 1; i < board.length; i++) {
             board[i] = String.valueOf(i);
         }
@@ -20,14 +20,20 @@ public class TicTacToeGame {
 
     public static void playerInput() {
         System.out.println("Please choose your letter for playing (x or o) \ntype letter & press enter");
-        String playLetter = scan.next();
+        String playLetter = sc.next();
         if (playLetter.equals("x")) {
-            System.out.println("You choose X \nnow computer will play with O");
+            System.out.println("You choose X \nnow computer will play with o");
         } else if (playLetter.equals("o")) {
-            System.out.println("You choose O \nnow computer will play with X");
+            System.out.println("You choose O \nnow computer will play with x");
         } else {
             System.out.println("Invalid Input \n");
             playerInput();
         }
+    }
+
+    public static void showBoard() {
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |\n-------------");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |\n-------------");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |\n-------------");
     }
 }
